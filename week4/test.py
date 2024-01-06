34ed313c454283623e2ec1affa62a7591121b654
rudxor02 <rudxor07@naver.com>
2024-01-06 02:41:39 +0000

    fix: test with stride

--- week4/test.py
+++ week4/test.py
@@ -1,11 +1,9 @@
 import math
-from functools import partial
-from random import randint
 
 import torch
 from tokenizers import Tokenizer
-from torch import IntTensor, LongTensor, nn
-from torch.utils.data import DataLoader
+from torch import IntTensor, nn
+from torch.utils.data import DataLoader, Dataset
 from torchtext.datasets import WikiText103
 
 from week4.config import config
@@ -13,42 +11,39 @@ from week4.model import GPT
 from week4.vocab import DATA_DIR_PATH, load_tokenizer
 
 
-def collate_fn(batch: list[str], tokenizer: Tokenizer) -> tuple[IntTensor, LongTensor]:
-    batch_tokens = tokenizer.encode_batch(batch)
+class WikiTextDataset(Dataset):
+    def __init__(self, tokenizer: Tokenizer, stride: int = 512):
+        self.stride = stride
+        _train, test = WikiText103(root=DATA_DIR_PATH, split=("train", "test"))
+        self.encodings = IntTensor(tokenizer.encode("\n\n".join(test)).ids)
 
-    batch_inputs, batch_labels = [], []
+    def __getitem__(self, index: int):
+        return (
+            self.encodings[index * self.stride : (index + 1) * self.stride],
+            self.encodings[
+                index * self.stride + 1 : (index + 1) * self.stride + 1
+            ].type(torch.LongTensor),
+        )
 
-    for tokens in batch_tokens:
-        tokens = tokens.ids
-        if len(tokens) <= config.block_size + 1:
-            tokens += [tokenizer.token_to_id("<pad>")] * (
-                config.block_size + 1 - len(tokens)
-            )
-            batch_inputs.append(tokens[:-1])
-            batch_labels.append(tokens[1:])
-            continue
-        idx = randint(0, len(tokens) - config.block_size - 1)
-        batch_inputs.append(tokens[idx : idx + config.block_size])
-        batch_labels.append(tokens[idx + 1 : idx + config.block_size + 1])
-
-    return IntTensor(batch_inputs), LongTensor(batch_labels)
+    def __len__(self):
+        return (len(self.encodings) // self.stride) - 1
 
 
 def test():
-    _train, test = WikiText103(root=DATA_DIR_PATH, split=("train", "test"))
-
     tokenizer = load_tokenizer()
 
+    dataset = WikiTextDataset(tokenizer=tokenizer, stride=config.block_size)
+
     dataloader = DataLoader(
-        list(test),
-        batch_size=config.batch_size,
-        collate_fn=partial(collate_fn, tokenizer=tokenizer),
-        num_workers=4,
-        shuffle=True,
+        dataset,
+        batch_size=32,
+        # collate_fn=partial(collate_fn, tokenizer=tokenizer),
+        # num_workers=4,
+        # shuffle=True,
     )
 
     model = GPT(config)
-    model.load_state_dict(torch.load("week4/data/model.v2.epoch_0.step_300"))
+    model.load_state_dict(torch.load("week4/data/model.v1.2.epoch_0.step_8000"))
 
     criterion = nn.CrossEntropyLoss(ignore_index=config.padding_idx)
 
@@ -63,6 +58,7 @@ def test():
             pred = torch.transpose(pred, 1, 2)
             loss = criterion(pred, label)
             losses.append(loss.item())
+            print(loss)
 
     print(f"cross entropy: {sum(losses) / (len(losses))}")
     print(f"perplexity: {math.exp(sum(losses) / (len(losses)))}")
