8b8e4e4d3d7a735926d70473fbb8f3f39eebdebf
rudxor02 <rudxor07@naver.com>
2024-02-03 03:06:25 +0000

    feat: plot results

--- week8/plot.py
+++ week8/plot.py
@@ -1,17 +1,32 @@
+import json
+from typing import Any
+
 import plotly.express as px
 
+from week8.config import config
 
-def plot_2d_values(x: list[str], y: list[str], values: list[list[float]]):
+
+def plot_2d_values(
+    x: list[str], y: list[str], values: list[list[float]], **kwargs: Any
+):
     if len(values) == 0:
         raise ValueError("No data to plot")
-    fig = px.imshow(values, x=x, y=y)
+    fig = px.imshow(values, x=x, y=y, **kwargs)
     fig.show()
 
 
-if __name__ == "__main__":
-    values = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+def plot_results(result_path: str):
+    with open(result_path, "r") as f:
+        values = json.load(f)
     plot_2d_values(
         x=list(map(str, range(len(values)))),
         y=list(map(str, range(len(values))))[::-1],
-        values=values,
+        values=values[::-1],
+        labels={"x": "LayerIndexFrom", "y": "LayerIndexTo", "color": "Accuracy"},
     )
+
+
+if __name__ == "__main__":
+    plot_results(config.antonyms_hypothesis_result_path)
+    # plot_results(config.en_es_hypothesis_result_path)
+    # plot_results(config.location_country_hypothesis_result_path)
